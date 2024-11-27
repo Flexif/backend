c287c91caecb4400795f42e29b9d5808a24f33a1
Salarific <salar.farsi@gmail.com>
2024-11-27 12:50:03 +0100

    Update server-side code

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ mongoose.set('strictQuery', false);
 mongoose.connect(process.env.DATABASE_URL)
     .then(() => {
         console.log('Connected to MongoDB');
-        app.listen(port, () => console.log(`Node.js application is running on port ${port}`));
+        // Bind to 0.0.0.0 instead of default localhost (127.0.0.1)
+        app.listen(port, '0.0.0.0', () => console.log(`Node.js application is running on port ${port}`));
     })
     .catch((err) => {
         console.error('Error connecting to MongoDB', err);
